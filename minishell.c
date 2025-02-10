33314f56d7c16835dc8ca24098d091d16b42b8b5
Lanila <109435712+Pakegen@users.noreply.github.com>
2025-02-10 01:35:40 +0100

    Add files via upload

---
+++ minishell.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qacjl <qacjl@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/12 12:43:52 by axbaudri          #+#    #+#             */
+/*   Updated: 2025/02/09 17:25:46 by qacjl            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	display_echo(t_prompt *prompt)
+{
+	char	**strs;
+
+	prompt->echo = exec_echo(prompt->cmd_line, prompt->strs);
+	{
+		strs = parse_echo(prompt);
+		ft_printf("join strings = %s\n", join_strings(strs));
+		ft_printf("%s", prompt->echo);
+	}
+	free(prompt->echo);
+}
+
+void	execute_command(t_shell *shell, t_prompt *prompt)
+{
+	if (!ft_strlen(prompt->cmd_line))
+		ft_printf("");
+	else if (ft_strcmp(prompt->strs[0], "echo") == 0
+		&& ft_strcmp(prompt->strs[1], "-n") == 0)
+		display_echo(prompt);
+	else if (ft_strcmp(prompt->strs[0], "export") == 0
+		&& count_words(prompt->cmd_line) == 1)
+		write_env(shell->export, prompt);
+	else if (ft_strcmp(prompt->strs[0], "env") == 0
+		&& count_words(prompt->cmd_line) == 1)
+		write_env(shell->env, prompt);
+	else if (ft_strcmp(prompt->strs[0], "cd") == 0
+		&& count_strings(prompt->strs) == 2)
+		exec_cd(shell, prompt);
+	else if (ft_strcmp(prompt->strs[0], "pwd") == 0
+		&& count_words(prompt->cmd_line) == 1)
+		ft_printf("%s\n", shell->pwd);
+	else if (ft_strcmp(prompt->strs[0], "unset") == 0
+		&& count_words(prompt->cmd_line) > 1)
+		exec_unset(shell, prompt);
+	else if (ft_strcmp(prompt->strs[0], "exit") == 0
+		&& count_words(prompt->cmd_line) == 1)
+		exit(1);
+	else
+		ft_printf("command not found: %s\n", prompt->strs[0]);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_shell		*shell;
+	t_prompt	*prompt;
+	const char	*buffer;
+
+	setup_signal();
+	(void)argv;
+	if (argc == 1)
+	{
+		shell = init_shell(envp);
+		buffer = readline("\033[0;36m> \033[0m");
+		while (buffer)
+		{
+			prompt = parse_prompt(buffer);
+			execute_command(shell, prompt);
+			free_prompt(prompt);
+			buffer = readline("\033[0;36m> \033[0m");
+			/*
+			if (!buffer)
+			{
+				write(2, "exit\n", 5);
+				break ;
+			}
+			*/
+		}
+		
+		free_prompt(prompt);
+		free_terminal(shell);
+	}
+	return (0);
+}
+
+/*int	main(int argc, char **argv, char **envp)
+{
+	t_shell		*shell;
+	int i = 0;
+
+	(void)argv;
+	if (argc == 1)
+	{
+		shell = init_shell(envp);
+		while (shell->var_names[i])
+			ft_printf("%s\n", shell->var_names[i++]);
+		free_terminal(shell);
+	}
+	return (0);
+}*/
---
+++ prototype_redirection.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   prototype_redirection.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qacjl <qacjl@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/10 01:25:41 by qacjl             #+#    #+#             */
+/*   Updated: 2025/02/10 01:33:29 by qacjl            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	handle_redirection(const char *file, int io_flag)
+{
+	int	fd;
+
+	if (io_flag == O_RDONLY)
+	{
+		fd = open(file, O_RDONLY);
+		if (fd < 0)
+		{
+			perror("Erreur lors de l'ouverture du fichier");
+			return (-1);
+		}
+		dup2(fd, STDIN_FILENO);
+	}
+	else
+	{
+		fd = open(file, io_flag, 0644);
+		if (fd < 0)
+		{
+			perror("Erreur lors de l'ouverture du fichier");
+			return (-1);
+		}
+		dup2(fd, STDOUT_FILENO);
+	}
+	close(fd);
+	return (0);
+}
+
+void	handle_pipe(char *cmd1[], char *cmd2[])
+{
+	int pipe_fd[2];
+	pid_t pid1, pid2;
+
+	if (pipe(pipe_fd) == -1)
+	{
+		perror("Erreur lors de la création du pipe");
+		exit(EXIT_FAILURE);
+	}
+	if ((pid1 = fork()) == 0)
+	{
+		close(pipe_fd[0]);
+		dup2(pipe_fd[1], STDOUT_FILENO);
+		close(pipe_fd[1]);
+		execve(cmd1[0], cmd1, NULL);
+		perror("Erreur lors de l'exécution de la commande");
+		exit(EXIT_FAILURE);
+	}
+	if ((pid2 = fork()) == 0)
+	{
+		close(pipe_fd[1]);
+		dup2(pipe_fd[0], STDIN_FILENO);
+		close(pipe_fd[0]);
+		execve(cmd2[0], cmd2, NULL);
+		perror("Erreur lors de l'exécution de la commande");
+		exit(EXIT_FAILURE);
+	}
+	close(pipe_fd[0]);
+	close(pipe_fd[1]);
+
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
+}
+
+int main2(void)
+		if (strstr(input, ">"))
+		{
+			char *cmd = strtok(input, "> ");
+			char *file = strtok(NULL, " ");
+			if (cmd && file)
+			{
+				if (handle_redirection(file, O_WRONLY | O_CREAT | O_TRUNC) == 0)
+				{
+					char *args[] = {cmd, NULL};
+					execve(cmd, args, NULL);
+				}
+			}
+		}
+		else if (strstr(input, "|"))
+		{
+			char *cmd1 = strtok(input, "|");
+			char *cmd2 = strtok(NULL, "");
+			if (cmd1 && cmd2)
+			{
+				char *args1[] = {cmd1, NULL};
+				char *args2[] = {cmd2, NULL};
+				handle_pipe(args1, args2);
+			}
+		}
